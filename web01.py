b501b1380ae8ef56139ac385b856b71a6d1fbac2
Paul Ramnora <pramnora@yahoo.com>
2018-10-24 15:00:17 +0100

    Update web01.py
    Renamed file: test01.html to become web01.html/also, updated each file reference.

--- web01.py
+++ web01.py
@@ -2,7 +2,7 @@
 
 # First, create a sub-directiory folder called: templates;
 # and, inside of the 'templates' folder...;
-# create a web page file called: test01.html.
+# create a web page file called: web01.html.
 
 # When you run the following code...; 
 # the 'test01.html' web page should, automatically, 
@@ -15,7 +15,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def showWebPage():
- return render_template("test01.html")
+ return render_template("web01.html")
 
 if __name__=="__main__":
  app.run()
